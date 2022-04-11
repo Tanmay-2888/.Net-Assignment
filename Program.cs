2d9ff59274375c010daec07134b93efb0c8b2c25
Tanmay-2888 <103040016+Tanmay-2888@users.noreply.github.com>
2022-04-11 16:56:33 +0530

    commit code

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _Net_Assignment
+{
+    class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"book Id :{Id} book name :{Name} Author name :{AuthorName} book price :{Price}";
+        }
+
+
+
+    
+        static void Main(string[] args)
+        {
+          Book book =new Book();
+          book.Id =1;
+          book.Name ="The Psychology of Money";
+          book.Price =399;
+          book.AuthorName ="Morgan Housel";
+
+          Console.WriteLine(book);
+
+        //   Console.WriteLine($"book id: {book.Id} Book name: {book.Name} Author name: {book.AuthorName} price is: {book.Price}");
+
+        //   Book b1 = new Book {Id =2, Name = "Rich Dad Poor Dad",AuthorName="Robert Kiyosaki & Sharon Lechter", Price =499};
+        //   Console.WriteLine($"book Id: {b1.Id} Book name: {b1.Name} Author name:{b1.AuthorName} Price is: { b1.Price}"); 
+        }
+    }
+}
